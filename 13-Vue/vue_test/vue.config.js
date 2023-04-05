ef82abd93b4084a4bb25cce8cbfef29c922f4833
18866805256 <3407085928@qq.com>
2023-04-05 20:07:46 +0700

    update

--- 13-Vue/vue_test/vue.config.js
+++ 13-Vue/vue_test/vue.config.js
@@ -1,5 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false /*关闭语法检查*/,
+  lintOnSave: false, //关闭语法检查
+  //开启代理服务器(方式一)
+  /* devServer: {
+    proxy:'http://localhost:5000'
+   },*/
+  //开启代理服务器(方式二)
+  devServer: {
+    proxy: {
+      '/atguigu': {
+        target: 'http://localhost:5000',
+        //ws: true,
+        //changeOrigin: true,
+      },
+    },
+  },
 })
