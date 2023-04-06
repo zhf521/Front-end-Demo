119e2cfe5f2e07efff9e1107f96172d3d95dc5f8
18866805256 <3407085928@qq.com>
2023-04-06 17:38:40 +0700

    update

--- 13-Vue/vue_test/vue.config.js
+++ 13-Vue/vue_test/vue.config.js
@@ -11,8 +11,15 @@ module.exports = defineConfig({
     proxy: {
       '/atguigu': {
         target: 'http://localhost:5000',
-        //ws: true,
-        //changeOrigin: true,
+        pathRewrite: { '^/atguigu': '' },
+        ws: true, //用于支持websocket
+        changeOrigin: false, //用于控制请求头中的host值
+      },
+      '/demo': {
+        target: 'http://localhost:5001',
+        pathRewrite: { '^/demo': '' },
+        //ws: true, //用于支持websocket
+        //changeOrigin: false, //用于控制请求头中的host值
       },
     },
   },
