f28b5d32e31926f1e4c46a3dae45c4197a478418
18866805256 <3407085928@qq.com>
2023-04-07 08:22:30 +0700

    update

---
+++ 13-Vue/vue_test/21-src-github搜索案例_vue-resource/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <Search></Search>
+    <List></List>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search'
+import List from './components/List'
+export default {
+  name: 'App',
+  components:{Search,List}
+}
+</script>
+
