0d5ee5aa27336e09bc694573e805a672c56a2224
18866805256 <3407085928@qq.com>
2023-04-11 08:49:24 +0800

    update

--- 13-Vue/vue_test/src/main.js
+++ 13-Vue/vue_test/src/main.js
@@ -2,15 +2,22 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-//引入ElementUI组件库
-import ElementUI from 'element-ui'
+//完整引入ElementUI组件库
+//import ElementUI from 'element-ui'
 //引入ElementUI全部样式
-import 'element-ui/lib/theme-chalk/index.css'
+//import 'element-ui/lib/theme-chalk/index.css'
+
+//按需引入
+import { Button,Row,DatePicker } from 'element-ui'
 
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 //应用ElementUI
-Vue.use(ElementUI)
+//Vue.use(ElementUI)
+Vue.component('el-button',Button)
+Vue.component('el-row',Row)
+Vue.component('el-date-picker',DatePicker)
+
 //创建vm
 new Vue({
   el: '#app',
