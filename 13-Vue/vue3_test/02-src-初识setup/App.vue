62719dc1ef8b6c4d78583a406e9b98a0828bcf69
18866805256 <3407085928@qq.com>
2023-04-18 15:15:37 +0800

    update

---
+++ 13-Vue/vue3_test/02-src-初识setup/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>一个人的信息</h1>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+import { h } from 'vue'
+export default {
+  name: 'App',
+  //此处只是测试一下setup，暂时不考虑响应式的问题
+  setup() {
+    //数据
+    let name = '张三'
+    let age = 18
+    //方法
+    function sayHello() {
+      alert(`我叫${name},我${age}岁了，你好啊！`)
+    }
+    // return {
+    //   name,
+    //   age,
+    //   sayHello
+    // }
+    //返回一个函数（渲染函数）
+    // return () => { return h('h1','尚硅谷') }
+  }
+}
+</script>
