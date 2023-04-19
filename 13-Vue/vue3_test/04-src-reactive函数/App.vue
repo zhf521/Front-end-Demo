54cb5f8cc2883b1f023db041fe440e426afd49b4
18866805256 <3407085928@qq.com>
2023-04-19 09:26:03 +0800

    update

--- 13-Vue/vue3_test/04-src-reactive函数/App.vue
+++ 13-Vue/vue3_test/04-src-reactive函数/App.vue
@@ -1,14 +1,11 @@
 <template>
   <h1>一个人的信息</h1>
-  <h2 v-show="person.name">姓名：{{ person.name }}</h2>
+  <h2>姓名：{{ person.name }}</h2>
   <h2>年龄：{{ person.age }}</h2>
-  <h2 v-show="person.sex">性别：{{ person.sex }}</h2>
   <h3>工作种类：{{ person.job.type }}</h3>
   <h3>工作薪水：{{ person.job.salary }}</h3>
   <h4>个人爱好：{{ person.hobby }}</h4>
   <button @click="changeInfo">修改人的信息</button>
-  <button @click="addSex">添加一个sex属性</button>
-  <button @click="deleteName">删除一个name属性</button>
 </template>
 
 <script>
@@ -33,18 +30,10 @@ export default {
       person.age = 38
       person.job.type = 'UI设计师'
     }
-    function addSex() {
-      person.sex='男'
-    }
-    function deleteName() {
-      delete(person.name)
-    }
     //返回一个对象（常用）
     return {
       person,
       changeInfo,
-      addSex,
-      deleteName,
     }
   }
 }
