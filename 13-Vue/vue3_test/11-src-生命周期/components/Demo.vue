20887fb74515660fd608ba215c4cadb8f69555c3
18866805256 <3407085928@qq.com>
2023-04-21 11:58:07 +0800

    update

---
+++ 13-Vue/vue3_test/11-src-生命周期/components/Demo.vue
@@ -0,0 +1,64 @@
+<template>
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我加一</button>
+</template>
+
+<script>
+import {
+  ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted
+} from "vue"
+export default {
+  name: "Demo",
+  setup() {
+    //数据
+    let sum = ref(0)
+    //通过组合式API的形式去使用生命周期钩子
+      onBeforeMount(() => {
+        console.log('---onBeforeMount---')
+      }),
+      onMounted(() => {
+        console.log('---onMounted---')
+      }),
+      onBeforeUpdate(() => {
+        console.log('---onBeforeUpdate---')
+      }),
+      onUpdated(() => {
+        console.log('---onUpdated---')
+      }),
+      onBeforeUnmount(() => {
+        console.log('---onBeforeUnmount---')
+      }),
+      onUnmounted(() => {
+        console.log('---onUnmounted---')
+      })
+    return { sum }
+  },
+  //通过配置项的形式使用生命周期钩子
+  /*beforeCreate() {
+    console.log('---beforeCreate---')
+  },
+  created() {
+    console.log('---Created---')
+
+  },
+  beforeMount() {
+    console.log('---beforeMount---')
+  },
+  mounted() {
+    console.log('---mounted---')
+  },
+  beforeUpdate() {
+    console.log('---beforeUpdate---')
+  },
+  updated() {
+    console.log('---Updated---')
+  },
+  beforeUnmount() {
+    console.log('---beforeUnmount---')
+  },
+  unmounted() {
+    console.log('---unmounted---')
+  },*/
+
+}
+</script>
