43fa2fca22ae3236e48e11a4eb48912e8aad7650
18866805256 <3407085928@qq.com>
2023-04-21 11:33:32 +0800

    update

--- 13-Vue/vue3_test/src/components/Demo.vue
+++ 13-Vue/vue3_test/src/components/Demo.vue
@@ -1,44 +1,64 @@
 <template>
   <h2>当前求和为：{{ sum }}</h2>
   <button @click="sum++">点我加一</button>
-  <hr>
-  <h2>当前的信息为：{{ msg }}</h2>
-  <button @click="msg += '!'">修改信息</button>
-  <hr>
-  <h2>姓名：{{ person.name }}</h2>
-  <h2>年龄：{{ person.age }}</h2>
-  <h2>薪资：{{ person.job.j1.salary }}</h2>
-  <button @click="person.name += '~'">修改姓名</button>
-  <button @click="person.age++">增长年龄</button>
-  <button @click="person.job.j1.salary++">涨薪</button>
 </template>
 
 <script>
-import { ref, reactive, watchEffect } from "vue"
+import {
+  ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted
+} from "vue"
 export default {
   name: "Demo",
   setup() {
     //数据
     let sum = ref(0)
-    let msg = ref('你好啊')
-    let person = reactive({
-      name: '张三',
-      age: 18,
-      job: {
-        j1: {
-          salary: 20
-        }
-      }
-    })
-
-    watchEffect(() => {
-      const x1 = sum.value
-      const x2 = person.job.j1.salary
-      console.log('watchEffect所指定的回调执行了')
-    })
-
-    //返回一个对象（常用）
-    return { sum, msg, person }
+    //通过组合式API的形式去使用生命周期钩子
+      onBeforeMount(() => {
+        console.log('---onBeforeMount---')
+      }),
+      onMounted(() => {
+        console.log('---onMounted---')
+      }),
+      onBeforeUpdate(() => {
+        console.log('---onBeforeUpdate---')
+      }),
+      onUpdated(() => {
+        console.log('---onUpdated---')
+      }),
+      onBeforeUnmount(() => {
+        console.log('---onBeforeUnmount---')
+      }),
+      onUnmounted(() => {
+        console.log('---onUnmounted---')
+      })
+    return { sum }
   },
+  //通过配置项的形式使用生命周期钩子
+  /*beforeCreate() {
+    console.log('---beforeCreate---')
+  },
+  created() {
+    console.log('---Created---')
+
+  },
+  beforeMount() {
+    console.log('---beforeMount---')
+  },
+  mounted() {
+    console.log('---mounted---')
+  },
+  beforeUpdate() {
+    console.log('---beforeUpdate---')
+  },
+  updated() {
+    console.log('---Updated---')
+  },
+  beforeUnmount() {
+    console.log('---beforeUnmount---')
+  },
+  unmounted() {
+    console.log('---unmounted---')
+  },*/
+
 }
 </script>
