67ae0f67fe9b8e1a337d80a9f46fc7b40b0b89d8
18866805256 <3407085928@qq.com>
2023-04-21 19:38:07 +0800

    update

--- 13-Vue/vue3_test/src/App.vue
+++ 13-Vue/vue3_test/src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <div class="app">
-    <h3>我是App组件（祖），{{ name }}--{{ price }}</h3>
-    <Child></Child>
-  </div>
+  <h3>我是App组件</h3>
 </template>
 <script>
-import Child from './components/Child.vue'
-import { reactive, toRefs, provide } from 'vue'
+import { readonly, ref, reactive, toRefs, isRef, isReactive, isReadonly, isProxy } from 'vue'
 export default {
   name: 'App',
-  components: { Child },
   setup() {
-    let car = reactive({
-      name: '奔驰',
-      price: '40W'
-    })
-    provide('car', car)//给自己的后代组件传递数据
+    let car = reactive({ name: '奔驰', price: '40W' })
+    let sum = ref(0)
+    let car2 = readonly(car)
+
+    console.log(isRef(sum))
+    console.log(isReactive(car))
+    console.log(isReadonly(car2))
+    console.log(isProxy(car))
+
     return { ...toRefs(car) }
   }
 }
