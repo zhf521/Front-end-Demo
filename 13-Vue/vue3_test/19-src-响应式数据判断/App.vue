1514c9d3215eca67ca8a69d22d7d0ad9af93861d
18866805256 <3407085928@qq.com>
2023-04-22 13:32:33 +0800

    update

---
+++ 13-Vue/vue3_test/19-src-响应式数据判断/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h3>我是App组件</h3>
+</template>
+<script>
+import { readonly, ref, reactive, toRefs, isRef, isReactive, isReadonly, isProxy } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    let car = reactive({ name: '奔驰', price: '40W' })
+    let sum = ref(0)
+    let car2 = readonly(car)
+
+    console.log(isRef(sum))
+    console.log(isReactive(car))
+    console.log(isReadonly(car2))
+    console.log(isProxy(car))
+
+    return { ...toRefs(car) }
+  }
+}
+</script>
+<style>
+.app {
+  background-color: gray;
+  padding: 10px;
+}
+</style>
