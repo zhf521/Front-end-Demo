6e06c677416230792e9d2316e4de4131b62ad4e5
18866805256 <3407085928@qq.com>
2023-04-22 14:34:57 +0800

    update

---
+++ 13-Vue/vue3_test/20-src-Teleport/components/Son.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="son">
+    <h3>我是Son组件（孙）</h3>
+    <Dialog></Dialog>
+  </div>
+</template>
+<script>
+import Dialog from './Dialog'
+export default {
+  name: 'Son',
+  components:{Dialog
+  }
+}
+</script>
+<style>
+.son {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
---
+++ 13-Vue/vue3_test/src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件（子）</h3>
+    {{ sum }}
+  </div>
+</template>
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Child',
+  setup() {
+    let sum = ref(0)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ sum })
+      }, 1000)
+    })
+  }
+}
+</script>
+<style>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
