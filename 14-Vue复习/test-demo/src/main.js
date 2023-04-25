ba7ec3a12f0ed088e7714bb7d1192e61343b29ea
18866805256 <3407085928@qq.com>
2023-04-25 21:25:25 +0800

    update

--- 14-Vue复习/test-demo/src/main.js
+++ 14-Vue复习/test-demo/src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store' // 引入store
 
 Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
   render: (h) => h(App),
-  beforeCreate() {
-    Vue.prototype.$bus = this // 安装全局事件总线，$bus 就是当前应用的 vm
-  },
+  store,
 })
