27a4fa950bec6d033881952c317d9024ef9570b9
18866805256 <3407085928@qq.com>
2023-04-25 09:23:48 +0800

    组件自定义事件

---
+++ 14-Vue复习/test-demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ msg }},学生的姓名是：{{ studentName }}</h1>
+    <Student @getName="getStudentName"></Student>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student'
+export default {
+  name: 'App',
+  components: {
+    Student
+  },
+  data() {
+    return {
+      msg: "你好啊！",
+      studentName: ''
+    }
+  },
+  methods: {
+    getStudentName(name) {
+      this.studentName = name
+    }
+  }
+}
+</script>
+
+<style></style>
