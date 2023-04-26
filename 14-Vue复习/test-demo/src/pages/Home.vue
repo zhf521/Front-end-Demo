fc38b6c06e12562bd45aefc081c919852714dec6
18866805256 <3407085928@qq.com>
2023-04-26 16:00:57 +0800

    路由

--- 14-Vue复习/test-demo/src/pages/Home.vue
+++ 14-Vue复习/test-demo/src/pages/Home.vue
@@ -1,5 +1,15 @@
 <template>
-  <h2>我是Home的内容</h2>
+  <div>
+    <h2>我是Home的内容</h2>
+    <div>
+      <router-link to="/home/news">News</router-link>
+      <br>
+      <router-link to="/home/message">Message</router-link>
+      <div>
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
