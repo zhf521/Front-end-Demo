a3853e9b023778cb9d251f851f03d8d72d3f6094
18866805256 <3407085928@qq.com>
2023-04-26 17:56:15 +0800

    update

---
+++ 14-Vue复习/test-demo/src/pages/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <ul>
+      <li>消息编号：{{ $route.query.id }}</li>
+      <li>消息标题：{{ $route.query.title }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Detail'
+}
+</script>
+<style></style>
