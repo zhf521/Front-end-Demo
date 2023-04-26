1a61c1e5d78eda2ec4c110a621ba687873dc0887
18866805256 <3407085928@qq.com>
2023-04-26 15:28:01 +0800

    update

--- 14-Vue复习/test-demo/src/main.js
+++ 14-Vue复习/test-demo/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store' // 引入store
+import VueRouter from 'vue-router' // 引入VueRouter
+import router from './router' // 引入路由器
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter) // 应用插件
 
 new Vue({
   el: '#app',
   render: (h) => h(App),
-  store,
+  router: router,
 })
