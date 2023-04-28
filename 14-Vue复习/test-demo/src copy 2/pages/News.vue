3f43d0129ee9e414b64609714a51bdae4f1593cf
18866805256 <3407085928@qq.com>
2023-04-28 11:22:26 +0800

    update

---
+++ 14-Vue复习/test-demo/src copy 2/pages/News.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="(news, index) in newsArr" :key="index">{{ news }} <input type="text"> </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name:'News',
+  data() {
+    return {
+      newsArr: ["news001", "news002", "news003", "news004"],
+    }
+  },
+}
+</script>
+<style></style>
