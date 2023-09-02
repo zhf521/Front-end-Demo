6d976c578680eddb5981283362406296e2f92f0c
zhf521 <3407085928@qq.com>
2023-09-02 09:33:00 +0800

    栈结构总结

--- 15-JS数据结构与算法/02-栈结构/index.js
+++ 15-JS数据结构与算法/02-栈结构/index.js
@@ -5,3 +5,7 @@ const item1 = stack.pop();
 const item2 = stack.pop();
 // 应用场景
 // 十进制转二进制、判断字符串的括号是否有效、函数调用堆栈
+// 技术要点
+// 栈是一个后进先出的数据结构
+// JavaScript中没有栈，但可以用Array实现栈的所有功能
+// 栈常用操作：push、pop、stack[stack.length - 1]
